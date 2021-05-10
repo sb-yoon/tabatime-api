3542b243e0c366e6b5a012d74b5f2a75a57eccba
soobinyoon <soobin.yoon@unlike.kr>
2021-05-10 18:29:33 +0900

    init

---
+++ src/main/java/kr/unlike/tabatime/TabatimeApplication.java
@@ -0,0 +1,13 @@
+package kr.unlike.tabatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabatimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabatimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/unlike/tabatime/TabatimeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.unlike.tabatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabatimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
